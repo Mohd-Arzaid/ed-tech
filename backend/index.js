64295da82f6bd11a778a607be914c49eab622c59
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-02-22 15:17:56 +0530

    mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDb from "./utils/db.js";
 
-const app = express();
 dotenv.config();
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// middlewares
+
+// parse the incoming request data in JSON format
 app.use(express.json());
+
+// parse the cookies in Js Object
+app.use(cookieParser());
 
 app.get("/", (req, res) =>
   res.status(200).json({
@@ -15,4 +23,7 @@ app.get("/", (req, res) =>
   })
 );
 
-app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
+app.listen(PORT, async () => {
+  connectDb();
+  console.log(`Server is running at port ${PORT}`);
+});
